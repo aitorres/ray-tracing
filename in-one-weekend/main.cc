2c0a8f207609d1fc1c6ee7527c7e50e81d751036
Andrés Ignacio Torres <andresitorresm@gmail.com>
2024-12-28 22:12:24 -0800

    normals

--- in-one-weekend/main.cc
+++ in-one-weekend/main.cc
@@ -7,7 +7,7 @@
 // Reference: https://raytracing.github.io/books/RayTracingInOneWeekend.html#overview
 
 // Checks if a ray hits a sphere of a given center and radius
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = center - r.origin();
 
     auto a = dot(r.direction(), r.direction());
@@ -15,14 +15,21 @@ bool hit_sphere(const point3& center, double radius, const ray& r) {
     auto c = dot(oc, oc) - radius * radius;
     auto discriminant = b*b - 4*a*c;
 
-    return discriminant >= 0;
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - std::sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 // Returns the color for a given scene ray
 color ray_color(const ray& r) {
     // Let's see if we hit a sphere!
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
 
     // Figuring out the ray direction
