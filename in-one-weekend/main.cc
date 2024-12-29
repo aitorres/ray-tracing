31b5f3d86158f34b4ca355065bb544a7cfef0765
Andrés Ignacio Torres <andresitorresm@gmail.com>
2024-12-28 22:31:22 -0800

    more headers

--- in-one-weekend/main.cc
+++ in-one-weekend/main.cc
@@ -47,7 +47,7 @@ int main() {
     // Note that this is an ideal ratio, not the actual one that will be used,
     // since we're working with integers
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 800;
+    const int image_width = 1200;
 
     // Calculate the image height, and ensure that it's at least 1
     int image_height = int(image_width / aspect_ratio);
